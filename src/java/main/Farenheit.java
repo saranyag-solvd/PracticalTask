490f85bcfbb5906064ab0ef23528d20aa79617de
Saranya Gopalakrishnan <sgopalakrishnan.laba@solvd.com>
2023-05-15 09:36:35 -0400

    Practical Task

---
+++ src/java/main/Farenheit.java
@@ -0,0 +1,13 @@
+public class Farenheit {
+    //Java program to convert the temperature in Celsius to Fahrenheit
+    public static void main(String[] args) {
+        double celsius = 10.0;
+        double farenheit =0.0;
+
+        //calculation
+        farenheit = (celsius *1.8)+35;
+        System.out.println("Value of temp in farenheit:"+ farenheit);
+
+    }
+
+}
