d57bba1e487e7b583b9a3866fc80a625e59d7baa
Saranya Gopalakrishnan <sgopalakrishnan.laba@solvd.com>
2023-05-15 09:34:07 -0400

    Practical Task

---
+++ src/java/main/Average.java
@@ -0,0 +1,19 @@
+public class Average {
+    // Java program to find out the average of a set of integers
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6};
+        float sum = 0;
+
+        //calculate sum
+        int i = 0;
+        while (i < num.length) {
+            sum = sum + num[i];
+
+            i++;
+
+        }
+        // calculate average
+        float average = (sum / num.length);
+        System.out.println("Average:" + average);
+    }
+    }
---
+++ src/java/main/Average1.java
@@ -0,0 +1,18 @@
+public class Average1 {
+//Java program to find the average of 5 numbers using a while loop
+    public static void main(String[] args) {
+        int []num = {1,2,3,4,5};
+        int sum = 0;
+        // calculate Sum
+        int i = 0;
+        while(i < num.length){
+            sum = sum + num[i];
+            i++;
+
+        }
+        //calculate average
+        float average=( sum / num.length);
+        System.out.println("average:"+ average);
+
+    }
+}
