e1a0a69f8622821a5a65a3d00744fbe06db32c59
Saranya Gopalakrishnan <sgopalakrishnan.laba@solvd.com>
2023-05-16 14:01:17 -0400

    Practical Task

--- src/java/main/RandomNumber.java
+++ src/java/main/RandomNumber.java
@@ -6,7 +6,6 @@ public class RandomNumber {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number");
         int num = input.nextInt();
-
         if (num % 2 == 0) {
             System.out.println("Number is even");
         } else
